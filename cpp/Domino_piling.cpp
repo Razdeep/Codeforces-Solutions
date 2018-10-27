bab26c3043f6aecfd6c27fd4b44100be6801aa99
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-10-27 08:25:38 +0530

    Added Domino_piling.cpp

---
+++ cpp/Domino_piling.cpp
@@ -0,0 +1,11 @@
+// https://codeforces.com/problemset/problem/50/A
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ll row,column;
+    cin>>row>>column;
+    cout<<(row*column)/2<<endl;
+    return 0;
+}
