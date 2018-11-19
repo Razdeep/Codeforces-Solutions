f832a62f8640e215b9e56f1e93789e0421b77f39
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-11-19 09:04:14 +0530

    Solved Minimizing_the_string.cpp

--- cpp/Minimizing_the_string.cpp
+++ cpp/Minimizing_the_string.cpp
@@ -1,6 +1,5 @@
 // https://codeforces.com/contest/1076/problem/A
 // Minimizing the string
-// UNSOLVED
 
 #include<bits/stdc++.h>
 #define MAX 200005
@@ -8,20 +7,19 @@ using namespace std;
 typedef long long ll;
 int main()
 {
-    int n,max_str=INT_MIN,i;
+    int n;
     char str[MAX];
     cin>>n>>str;
-    for(i=0;i<n;i++)
-    {
-        max_str=(str[i]>max_str)?str[i]:max_str;
-    }
     bool done=false;
-    for(i=0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
-        if(str[i]==max_str && !done)
+        if(!done)
         {
-            done=true;
-            continue;
+            if( str[i]>str[i+1] || i==n-1 )
+            {
+                done=true;
+                continue;
+            }
         }
         printf("%c",str[i]);
     }
