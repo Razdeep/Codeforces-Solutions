ddf7d8093952826c8d8e7dbe7679a8d3a1aae94b
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2018-12-15 23:14:11 +0530

    Added Letters_rearranging.cpp

--- cpp/Letters_rearranging.cpp
+++ cpp/Letters_rearranging.cpp
@@ -1,5 +1,5 @@
 // https://codeforces.com/contest/1093/problem/B
-// NOT SOLVED
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -12,38 +12,8 @@ int main()
         string s;
         cin.ignore();
         cin>>s;
-        int warned=false;
-        int impossible=false;
-        int odd_index,even_index;
-        for(int i=0;i<s.size();i++)
-        {
-            int current_char_frequency=count(s.begin(),s.end(),s[i]);
-            if( warned==false && current_char_frequency%2==1 )
-            {
-                odd_index=i;
-                warned=true;
-            }
-            else if( warned && current_char_frequency )
-            {
-                impossible=true;
-                break;
-            }
-            else if( current_char_frequency%2==0 )
-            {
-                even_index=i;
-            }
-        }
-        if(impossible)
-        {
-            cout<<"-1"<<endl;
-            continue;
-        }
-        //Replace oddindex with even
-        char temp=s[odd_index];
-        s[odd_index]=s[even_index];
-        s[even_index]=temp;
-        cout<<s<<endl;
+        sort(s.begin(),s.end());
+        cout<<((s[0]==s[s.size()-1])?"-1":s)<<endl;
     }
-
     return 0;
 }
