8a42f9e661bd56d4e237e5cd457a7adb0c3bb0a8
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2019-01-02 11:48:53 +0530

    Solved Ehab_and_subtraction.cpp

--- cpp_2018/Ehab_and_subtraction.cpp
+++ cpp_2018/Ehab_and_subtraction.cpp
@@ -1,45 +1,38 @@
 // https://codeforces.com/contest/1088/problem/B
-// NOT SOLVED
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
+#define all(v) v.begin(), v.end()
 using namespace std;
 typedef long long ll;
 int main()
 {
-    int n,k;
-    cin>>n>>k;
-    vector<int> arr;
-    for(int i=0;i<n;i++)
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n, 0);
+    for (int i = 0; i < n; i++)
     {
-        int this_number;
-        cin>>this_number;
-        arr.push_back(this_number);
+        cin >> arr[i];
     }
-    sort(arr.begin(),arr.end());
-    
-    // Finding the unique vector
-    vector<int> new_arr;
-    for(int i=0;i+1<arr.size();i++)
-    {
-        if(arr[i]!=arr[i+1])
-        {
-            new_arr.push_back(arr[i]);
-        }
-    }
+    sort(all(arr));
 
-    int deducted_so_far=0;
-    int max_count_unique=new_arr.size();
-    for(int i=0;i<k;i++)
+    // Finding the unique vector
+    auto ptr = std::unique(all(arr));
+    arr.resize(std::distance(arr.begin(), ptr));
+
+    int deducted_so_far = 0;
+    int max_count_unique = arr.size();
+    for (int i = 0; i < k; i++)
     {
-        if(i>=max_count_unique)
+        if (i >= max_count_unique)
         {
-            cout<<"0"<<endl;
+            cout << 0 << endl;
+            continue;
         }
-        else
-        {
-            int answer= (new_arr[i]-deducted_so_far);
-            cout<<answer<<endl;
-            deducted_so_far+=new_arr[i];
-        }
+        int answer = arr[i] - deducted_so_far;
+        deducted_so_far += answer;
+        cout << answer << endl;
     }
     return 0;
 }
