a1b36af63253e8e58ecdb9eb9a1ba28a8e576553
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2019-04-02 19:39:50 +0530

    Solved Cashier.cpp

--- cpp_2018/Cashier.cpp
+++ cpp_2018/Cashier.cpp
@@ -1,34 +1,27 @@
 // https://codeforces.com/contest/1059/problem/A
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
 using namespace std;
 int main()
 {
-    int count=0;
-    int n,L,a;
-    cin>>n>>L>>a;
-    if(n==0)
+    int count = 0;
+    int no_of_customers, total_work_duration, break_duration;
+    cin >> no_of_customers >> total_work_duration >> break_duration;
+    vector<int> customerEntryTime(no_of_customers, -1), customerDuration(no_of_customers, -1);
+    for (int i = 0; i < no_of_customers; i++)
+        cin >> customerEntryTime[i] >> customerDuration[i];
+    int freed_at = 0;
+    for (int i = 0; i < no_of_customers; i++)
     {
-        cout<<(L/a)<<endl;
-        return 0;
+        int idle_time = customerEntryTime[i] - freed_at;
+        count += idle_time / break_duration;
+        freed_at = customerEntryTime[i] + customerDuration[i];
     }
-    int t[n],l[n];
-    int i;
-    for(i=0;i<n;i++)
-    {
-        cin>>t[i]>>l[i];
-    }
-    // Block time
-    i--;
-    count+=((L-(t[i]+l[i]))/a);
-    // Dokan Kholar somoy
-    if(t[0]>a)
-    count+=(t[0]/a);
-    for(i=0;i<n-1;i++)
-    {
-        if((t[i+1]-(t[i]+l[i]))/a)
-        count+=((t[i+1]-(t[i]+l[i]))/a);
-    }
-    cout<<count<<endl;
+    int last_idle_time = total_work_duration - freed_at;
+    count += last_idle_time / break_duration;
+    cout << count << endl;
     return 0;
 }
