e1fe87bed15d46e397393ff6ef561852246d424a
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2019-11-20 16:45:26 +0530

    Added Educational Round Div 2 #67

---
+++ contests/Educational Round Div 2 #67/Vasya_and_array.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/contest/1187/problem/C
+// NOT SOLVED
+#include <bits/stdc++.h>
+#define For(i, n) for (__typeof(i) i = 0; i < n; i++)
+#define all(v) v.begin(), v.end()
+typedef long long ll;
+using namespace std;
+int main()
+{
+	ll n, m, i;
+	cin >> n >> m;
+	vector<ll> t(m, 0LL), l(m, 0LL), r(m, 0LL);
+	For (i, m)
+	{
+		cin >> t[i] >> l[i] >> m[i];
+		
+	}
+	return 0;
+}
